f171aef283e9f4c7b59bd483ed1f7c056fc9efc1
dk949 <56653556+dk949@users.noreply.github.com>
2022-09-15 17:41:40 +0300

    actually parsing git status now

--- source/hooks/git.d
+++ source/hooks/git.d
@@ -1,5 +1,6 @@
 module hooks.git;
 import std.bitmanip;
+import std.algorithm;
 import std.process;
 import std.string;
 import std.regex;
@@ -7,7 +8,6 @@ import std.functional;
 import std.conv;
 import std.stdio;
 import common;
-
 
 static assert(GitStatus.sizeof == 2);
 struct GitStatus {
@@ -36,24 +36,54 @@ struct GitStatus {
             int, "", 7
     )
     );
+    string toString() const {
+        return text("GitStatus(", '\n',
+            "\tadded = ", added, '\n',
+            "\tmodified = ", modified, '\n',
+            "\trenamed = ", renamed, '\n',
+            "\tdeleted = ", deleted, '\n',
+            "\tuntracked = ", untracked, '\n',
+            "\tremoteState = ", remoteState, '\n',
+            "\tunmerged = ", unmerged, '\n',
+            "\tstashed = ", stashed, '\n',
+            ')'
+        );
+    }
 }
 
-// TODO
 GitStatus getGitStatus() {
+    /*
+        The expressions used to pare git status are taken directly from
+        spaceship prompt, and hence are licensed by them under the MIT license
+        (see README for more detail).
+    */
+    static immutable untrackedRE = ctRegex!(`^\?\?`, "m");
+    static immutable stagedREs = [
+        ctRegex!(`^A[ MDAU] `, "m"),
+        ctRegex!(`^M[ MD] `, "m"),
+        ctRegex!(`^UA`, "m"),
+    ];
+    static immutable modifiedRE = ctRegex!(`^[ MARC]M `, "m");
+    static immutable renamedRE = ctRegex!(`^R[ MD] `, "m");
+    static immutable deletedREs = [
+        ctRegex!(`^[MARCDU ]D `, "m"),
+        ctRegex!(`^D[ UM] `, "m"),
+    ];
+    static immutable unmergedREs = [
+        ctRegex!(`^U[UDA] `, "m"),
+        ctRegex!(`^AA `, "m"),
+        ctRegex!(`^DD `, "m"),
+        ctRegex!(`^[DA]U `, "m"),
+    ];
+
     GitStatus output;
     const s = git("status", "--porcelain");
-    static immutable untrackedRE = ctRegex!(`^??`);
-    static immutable addedRE = ctRegex!(`^(?:(?:A[ MDAU] )|(?:M[ MD] ))|(?:UA)`);
-    static immutable modifiedRE = ctRegex!(`^[ MARC]M `);
-    static immutable renamedRE = ctRegex!(`^R[ MD] `);
-    static immutable deletedRE = ctRegex!(`^(?:[MARCDU ]D )|(?:D[ UM] )`);
-    static immutable unmergedRE = ctRegex!(`(?:(?:(?:U[UDA] )|(?:AA ))|(?:DD ))|(?:[DA]U )`);
 
     // Check for untracked files
     output.untracked = !!s.matchFirst(untrackedRE);
 
     // Check for staged files
-    output.added = !!s.matchFirst(addedRE);
+    output.added = any!(r => s.matchFirst(r))(stagedREs);
 
     // Check for modified files
     output.modified = !!s.matchFirst(modifiedRE);
@@ -62,10 +92,10 @@ GitStatus getGitStatus() {
     output.renamed = !!s.matchFirst(renamedRE);
 
     // Check for deleted files
-    output.deleted = !!s.matchFirst(deletedRE);
+    output.deleted = any!(r => s.matchFirst(r))(deletedREs);
 
     // Check for unmerged files
-    output.unmerged = !!s.matchFirst(unmergedRE);
+    output.unmerged = any!(r => s.matchFirst(r))(unmergedREs);
 
     //Check for stashes
     output.stashed = !!git("rev-parse", "--verify", "refs/stash");
@@ -93,7 +123,6 @@ GitStatus getGitStatus() {
         output.remoteState = GitStatus.RemoteState.Ahead;
     else if (behind)
         output.remoteState = GitStatus.RemoteState.Behind;
-
     return output;
 }
 
@@ -107,7 +136,7 @@ string git(S...)(S cmd) {
     const res = execute(["git", cmd]);
     if (res.status != 0)
         return null;
-    return res.output.strip;
+    return res.output.stripRight;
 }
 
 string getGitBranch() {
