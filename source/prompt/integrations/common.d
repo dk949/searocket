84ce77e9470e7a40db1595255349bc872397c5f9
dk949 <56653556+dk949@users.noreply.github.com>
2023-01-12 01:59:59 +0200

    handle non existent cwd
    this can happen if the current diractory gets deleted (e.g. from another shell)

--- source/prompt/integrations/common.d
+++ source/prompt/integrations/common.d
@@ -2,8 +2,27 @@ module prompt.integrations.common;
 import std.file;
 import std.path;
 
+
+nothrow
+string getCwd() {
+    import core.sys.posix.unistd: getcwd;
+    import core.stdc.errno: errno, ERANGE;
+    import std.exception: assumeUnique;
+    auto buf = new char[1024];
+    while(getcwd(buf.ptr, buf.length) == null)
+        if (errno == ERANGE)
+            // Note: could GC.free() the buffer, but it probably doesn't matter since paths don't get that long
+            buf = new char[buf.length * 2];
+        else
+            return null;
+
+    return buf.assumeUnique; // it is unique
+}
+
 auto findFile(alias Fn)() {
-    string dir = getcwd;
+    alias Ret = typeof(Fn(declval!DirEntry));
+    auto dir = getCwd;
+    if(dir is null) return Ret.init;
     string nextDir;
     while ((nextDir = dirName(dir)) != rootName(dir)) {
         foreach (DirEntry d; dirEntries(dir, SpanMode.shallow)) {
@@ -13,7 +32,6 @@ auto findFile(alias Fn)() {
         }
         dir = nextDir;
     }
-    alias Ret = typeof(Fn(declval!DirEntry));
     return Ret.init;
 }
 
