df2b3fe60b5046110f6465b4de5781c0a1f32e3a
dk949 <56653556+dk949@users.noreply.github.com>
2023-03-28 00:16:32 +0100

    switched to standard library getcwd

--- source/prompt/integrations/common.d
+++ source/prompt/integrations/common.d
@@ -2,27 +2,11 @@ module prompt.integrations.common;
 import std.file;
 import std.path;
 
-
-nothrow
-string getCwd() {
-    import core.sys.posix.unistd: getcwd;
-    import core.stdc.errno: errno, ERANGE;
-    import std.exception: assumeUnique;
-    auto buf = new char[1024];
-    while(getcwd(buf.ptr, buf.length) == null)
-        if (errno == ERANGE)
-            // Note: could GC.free() the buffer, but it probably doesn't matter since paths don't get that long
-            buf = new char[buf.length * 2];
-        else
-            return null;
-
-    return buf.assumeUnique; // it is unique
-}
-
 auto findFile(alias Fn)() {
     alias Ret = typeof(Fn(declval!DirEntry));
-    auto dir = getCwd;
-    if(dir is null) return Ret.init;
+    auto dir = getcwd;
+    if (dir is null)
+        return Ret.init;
     string nextDir;
     while ((nextDir = dirName(dir)) != rootName(dir)) {
         foreach (DirEntry d; dirEntries(dir, SpanMode.shallow)) {
@@ -43,6 +27,7 @@ string versionString(string exe, string param = "--version")() {
     import std.process;
     import std.string;
     import common;
+
     try {
         const e = execute([exe, param]);
         if (e.status != 0)
